e873664ace0bd782ddb549a028d087845de8f4df
seojeonghyeon <seojeonghyeon0630@gmail.com>
2021-11-25 11:31:09 +0900

    21.11.25

--- src/main/java/Test/Main.java
+++ src/main/java/Test/Main.java
@@ -7,56 +7,24 @@ public class Main {
 
     public void solution(){
         long beforeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
-        int result = solution("aabbaccc");
+        int result = solution(new int[][]{{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}});
         System.out.println(result);
         long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
         long secDiffTime = (afterTime - beforeTime); //두 시간에 차 계산
         System.out.println("시간차이(ms) : "+secDiffTime);
     }
-    public int solution(String s) {
+    public int solution(int[][] triangle) {
         int answer = 0;
-        String answerString = "";
-        if(s.length() < 2){
-            return s.length();
-        }
-        char[] sArray = s.toCharArray();
-        answer = sArray.length;
-        for(int i = 1; i < (sArray.length/2)+1; i++){
-            int point = 0;
-            while(point < sArray.length){
-                point = point + i;
-            }
-
-        }
 
         return answer;
     }
-    public String compress(String str, int increaseSize){
-        int finalLength = countCompression(str);
-        if(finalLength >= str.length()) return str;
-        StringBuilder compressed = new StringBuilder(finalLength);
-        int countConsecutive = 0;
-        for(int i =0; i<str.length(); i++){
-            countConsecutive++;
-            if(i+1 > str.length() || str.charAt(i) != str.charAt(i+1)){
-                compressed.append(countConsecutive);
-                compressed.append(str.charAt(i));
-                countConsecutive=0;
-            }
-        }
-        return compressed.toString();
+    public int dynamic(int[][] triangle, int row, int col){
+
     }
-    public int countCompression(String str){
-        int compressedLength = 0;
-        int countConsecutive = 0;
-        for(int i=0; i<str.length(); i++){
-            countConsecutive++;
-            if(i+1 >= str.length() || str.charAt(i) != str.charAt(i+1)){
-                compressedLength += 1 + String.valueOf(countConsecutive).length();
-                countConsecutive=0;
-            }
-        }
-        return compressedLength;
+    class Node{
+        int value;
+        boolean vistied;
+        
     }
 
 
