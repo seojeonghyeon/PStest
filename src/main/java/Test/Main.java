978df3cd6bf8d6d27e1a74351e421b9806f7fd20
seojeonghyeon <seojeonghyeon0630@gmail.com>
2021-12-19 12:42:35 +0900

    21.12.19

--- src/main/java/Test/Main.java
+++ src/main/java/Test/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public void solution(){
         long beforeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
-        int result = solution(new int[]{-16, 27, 65, -2, 58, -92, -71, -68, -61, -33});
+        int result = solution(5, new int[][]{{1,5},{2,5},{3,5},{4,5}});
 //        for(int i : result)
             System.out.println(result);
         long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
@@ -15,49 +15,75 @@ public class Main {
         System.out.println("시간차이(ms) : "+secDiffTime);
     }
 
-    class Node{
-        public int N, leftMin, rightMin;
-        public Node(){
-            this.N=Integer.MAX_VALUE;
-            this.leftMin = Integer.MAX_VALUE;
-            this.rightMin = Integer.MAX_VALUE;
+    int[][] nArray;
+    Boolean[] check;
+    public int solution(int n, int[][] edges) {
+        int answer = 0;
+        nArray = new int[n+1][n+1];
+        check = initBoolean(n);
+        for(int i=0; i<edges.length; i++){
+            nArray[edges[i][0]][edges[i][1]] = 1;
+            nArray[edges[i][1]][edges[i][0]] = 1;
         }
-        public Node(int N, int Min, int i){
-            this.N=N;
-            if(i == 0){
-                this.leftMin = Min;
-                this.rightMin = Integer.MAX_VALUE;
-            }else{
-                this.leftMin = Integer.MAX_VALUE;
-                this.rightMin = Min;
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(i==j) continue;
+                if(nArray[i][j]!=0) continue;
+                cleanBoolean();
+                check[i]=true;
+                dfs(i, i, j,1);
+            }
+        }
+        for(int a=1; a<nArray.length; a++){
+            for(int b=a+1; b<nArray.length; b++){
+                for(int c=b+1; c<nArray.length; c++){
+                    int mid = f(a,b,c);
+                    if(answer < mid) answer = mid;
+                }
+            }
+        }
+        return answer;
+    }
+    public int f(int a, int b, int c){
+        int[] n = new int[3];
+        n[0]=nArray[a][b];
+        n[1]=nArray[b][c];
+        n[2]=nArray[c][a];
+        Arrays.sort(n);
+        return n[1];
+    }
+    public void dfs(int cur, int i, int j, int level){
+        if(nArray.length <= i || nArray.length <= j) return;
+        if(nArray[cur][j]!=0){
+            nArray[i][j]=level;
+            nArray[j][i]=level;
+            return ;
+        }
+        if(nArray[i][cur]==0){
+            nArray[i][cur]=level;
+            nArray[cur][i]=level;
+        }
+        for(int k=1; k<nArray.length; k++){
+            if(!check[k] && nArray[cur][k]==1){
+                check[k]=true;
+                if(k==j) {
+                    nArray[i][j]=level+1;
+                    nArray[j][i]=level+1;
+                    return;
+                }
+                dfs(k, i, j, level+1);
             }
         }
     }
-    public int solution(int[] a) {
-        if(a.length <= 3) return a.length;
-        int lt = 0;
-        int rt = a.length-1;
-        int answer = 2;
-        Node[] nodes = new Node[a.length];
-        nodes[0] = new Node(a[0], a[0], 0);
-        nodes[a.length-1] = new Node(a[a.length-1],a[a.length-1],a.length-1);
-
-        for(int i=1; i<a.length-1; i++) nodes[i] = new Node();
-        while(lt < a.length){
-            if(rt==0) nodes[rt].rightMin = nodes[rt + 1].rightMin > nodes[rt].N ? nodes[rt].N : nodes[rt + 1].rightMin;
-            if(lt==a.length-1) nodes[lt].leftMin = nodes[lt - 1].leftMin > nodes[lt].N ? nodes[lt].N : nodes[lt - 1].leftMin;
-            if(lt != 0 || rt!=a.length-1){
-                nodes[lt].N = a[lt];
-                nodes[lt].leftMin = nodes[lt - 1].leftMin < nodes[lt].N ? nodes[lt - 1].leftMin : nodes[lt].N;
-                nodes[rt].N = a[rt];
-                nodes[rt].rightMin = nodes[rt + 1].rightMin < nodes[rt].N ? nodes[rt + 1].rightMin : nodes[rt].N;
-            }
-            lt++;
-            rt--;
-        }
-        for(int i=1; i<a.length-1; i++)
-            if(!(nodes[i-1].leftMin < nodes[i].N && nodes[i+1].rightMin < nodes[i].N)) answer++;
-        return answer;
+    public void cleanBoolean(){
+        for(int i =0; i<check.length; i++)
+            check[i]=false;
+    }
+    public Boolean[] initBoolean(int n){
+        Boolean[] booleans = new Boolean[n+1];
+        for(int i=0; i<=n; i++)
+            booleans[i]=false;
+        return booleans;
     }
 
     public static void main(String[] args) throws IOException {
