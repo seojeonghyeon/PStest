16e77c8de62c991fac40dc595f7cce653c6b2da6
seojeonghyeon <seojeonghyeon0630@gmail.com>
2022-01-12 12:36:38 +0900

    21.1.12

--- src/main/java/Test/Main.java
+++ src/main/java/Test/Main.java
@@ -7,29 +7,28 @@ public class Main {
 
     public void solution(){
         long beforeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
-        int result = solution(1);
+        int result = solution(9, new Integer[]{2,3,7});
 //        for(String i : result)
             System.out.println(result);
         long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
         long secDiffTime = (afterTime - beforeTime); //두 시간에 차 계산
         System.out.println("시간차이(ms) : "+secDiffTime);
     }
-
-    class Node{
-        int a;
-        Node(int a){
-            this.a=a;
-        }
+    public int solution(int finalScore, Integer[] scores) {
+        List<Integer> individualPlayScores = new ArrayList<Integer>(Arrays.asList(scores));
+        return numCombinationsForFinalScore(finalScore, individualPlayScores);
     }
-    public int solution(int a) {
-        Node node1 = new Node(1);
-        Node node2 = new Node(1);
-        Node node3 = node1;
-        if(node1.equals(node2)) System.out.println("node1 equals node2");
-        if(node1==node2) System.out.println("node1 == node2");
-        if(node1.equals(node3)) System.out.println("node1 equals node3");
-        if(node1==node3) System.out.println("node1 == node3");
-        return 0;
+    public static int numCombinationsForFinalScore(int finalScore, List<Integer> individualPlayScores){
+        int[] numCombinationsForScore = new int[finalScore+1];
+        for(int i=0; i<individualPlayScores.size(); ++i){
+            numCombinationsForScore[0]=1;
+            for(int j=1; j<=finalScore; ++j){
+                int withoutThisPlay = i-1 >= 0 ? numCombinationsForScore[j] : 0;
+                int withThisPlay = j >= individualPlayScores.get(i) ? numCombinationsForScore[j-individualPlayScores.get(i)] : 0;
+                numCombinationsForScore[j] = withoutThisPlay + withThisPlay;
+            }
+        }
+        return numCombinationsForScore[finalScore];
     }
 
 
