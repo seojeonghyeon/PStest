bad57fdd58e7a1a5477d72c6538ca6817d29c2ad
seojeonghyeon <seojeonghyeon0630@gmail.com>
2022-01-19 19:22:46 +0900

    22.1.19

--- src/main/java/optimumSubjectToCapactiy/Main.java
+++ src/main/java/optimumSubjectToCapactiy/Main.java
@@ -1,7 +1,9 @@
-package Test;
+package optimumSubjectToCapactiy;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
